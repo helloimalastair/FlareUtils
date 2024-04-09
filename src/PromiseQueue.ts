b223d82853e1c0773b6028e73f74066f085e53e8
helloimalastair <37487746+helloimalastair@users.noreply.github.com>
2024-04-09 12:53:44 +0200

    downgrade to non-experimental, update devDeps

--- src/PromiseQueue.ts
+++ src/PromiseQueue.ts
@@ -22,7 +22,7 @@ export class PromiseQueue {
 					return i;
 				}),
 			);
-			void(this.queue = this.queue.splice(completed, 1));
+			void (this.queue = this.queue.splice(completed, 1));
 		}
 	}
 
